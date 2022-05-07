62cb5e8d667f86e7288d16a9686f8c2b782c51a1
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2022-05-07 20:00:34 +0530

    about sectione done

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,73 @@
 
 export default function Footer() {
   return (
-    <div className="flex flex-col bg-black text-white pb-14 pt-14 h-128">
-        <div className="mb-14">
+    <div>
+    <div className="flex flex-row bg-black text-white pb-14 pt-14 h-fit items-center justify-center text-start">
+        <div className="mb-14 grid grid-cols-4 gap-20 px-20">
+          <div className="flex flex-col gap-2">
             <h1 className="text-white text-3xl text-left">
-                About Us
+              Online Shopping
             </h1>
+            <p>
+              Delivery Information and FAQ's
+            </p>
+            <p>
+              Terms and conditions
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
             <h1 className="text-white text-3xl text-left">
-                Contact
+              Store Areas
             </h1>
+            <p>
+              New Products
+            </p>
+            <p>
+              Special Offers
+            </p>
+            <p>
+              Trade Coffee?
+            </p>
+            <p>
+              Trade Coffee/White Label
+            </p>
+            <p>
+              Signature blend
+            </p>
+            <p>
+              Blog
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-white text-3xl text-left">
+              Brewing Guide
+            </h1>
+            <p>
+              Brew Guide
+            </p>
+            <p>
+              The Coffee Bean Journey
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-white text-3xl text-left">
+              About us
+            </h1>
+            <p>
+              About CafeMugShop
+            </p>
+            <p>
+              Terms of Service
+            </p>
+            <p>
+              Refund policy
+            </p>
+          </div>
         </div>
-    </div>
+        </div>
+        <div className= "bg-black text-white pb-4 pt-4 h-fit items-center justify-center text-center">
+          <p>© 2022 CafeMugs . All Rights reserved</p>
+        </div>
+      </div>
   )
 }
--- components/Hero.js
+++ components/Hero.js
@@ -12,12 +12,12 @@ export default function Hero() {
                 We serve all the best kinds of coffee around the world. 
                 </h5>
                 <div className="mb-2">
-                <button className="mt-5 px-5 py-4 rounded-md text-lg font-medium text-[#7A3636] bg-white hover:bg-indigo-800 active:bg-grey-900 focus:outline-none transition-all w-56">
-                    Start Brewing
-                </button>
+                    <button className="mt-5 px-5 py-4 rounded-md text-lg font-bold text-[#7A3636] bg-white hover:bg-red-300 hover:text-black active:bg-grey-900 focus:outline-none transition-all w-56">
+                        Start Brewing
+                    </button>
                 </div>
             </div>
-            <div className="flex-1 max-w-xl">
+            <div className="flex-1 max-w-2xl">
                 <img src="/cup.svg" alt="coffee"/>
             </div>
             </div>
