3150168e20db3bbe8b0f0d0606d232b915f903ac
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-06-19 16:02:22 +0530

    Banner revamp

--- components/Hero.js
+++ components/Hero.js
@@ -2,13 +2,13 @@ import Image from "next/image"
 export default function Hero() {
   return (
     <div className="">
-        <div className="container mx-auto px-5 lg:px-20">
-            <div className="grid py-10 md:grid md:grid-cols-6 justify-center items-center min-h-screen">
+        <div className="container mx-auto px-5 lg:px-20 mb-20">
+            <div className="grid pt-20 md:grid md:grid-cols-6 justify-center items-center min-h-screen">
                 <div className="md:col-span-3">
-                    <h1 className=" flex text-4xl md:text-5xl xl:text-6xl font-bold text-black text-left max-w-2xl mb-10">
+                    <h1 className=" flex text-5xl md:text-5xl xl:text-6xl font-bold text-black text-left max-w-2xl mb-10">
                         ENJOY WORLD’S FINEST COFFEE !
                     </h1>
-                    <h5 className="flex text-md text-xl xl:text-2xl font-bold text-[#C1C1C1] text-left">
+                    <h5 className="flex text-md text-3xl xl:text-2xl font-bold text-[#C1C1C1] text-left">
                         We serve all the best kinds of coffee around the world. 
                     </h5>
                     <div className="mb-2">
