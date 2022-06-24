e8a1d6323f1447976edbf2dcdde44fe59f6e5e96
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-06-24 17:58:14 +0530

    banner edit

--- components/Hero.js
+++ components/Hero.js
@@ -3,9 +3,9 @@ export default function Hero() {
   return (
     <div className="">
         <div className="container mx-auto px-5 lg:px-20 mb-20">
-            <div className="grid pt-20 md:grid md:grid-cols-6 justify-center items-center min-h-screen">
+            <div className="flex flex-col gap5 pt-20 md:grid md:grid-cols-6 justify-center items-center min-h-screen">
                 <div className="md:col-span-3">
-                    <h1 className=" flex text-5xl md:text-5xl xl:text-6xl font-bold text-black text-left max-w-2xl mb-10">
+                    <h1 className=" flex text-5xl md:text-5xl xl:text-7xl font-bold text-black text-left max-w-2xl mb-10">
                         ENJOY WORLD’S FINEST COFFEE !
                     </h1>
                     <h5 className="flex text-md text-3xl xl:text-2xl font-bold text-[#C1C1C1] text-left">
@@ -17,8 +17,8 @@ export default function Hero() {
                         </button>
                     </div>
                 </div>
-                <div className="md:col-span-3 max-w-2xl">
-                    <img src="/banner.svg" alt="coffee"/>
+                <div className="p-10 md:p-0 md:col-span-3">
+                    <Image src="/banner2.svg" width={2000} height={2000} alt="coffee"/>
                 </div>
             </div>
         </div>
